c30b4d3150d9668f47590ec7dbf576b609d9e107
thecodingbob <robertobiondo353@gmail.com>
2026-06-24 17:57:09 +0200

    Fix flickering copyright screen when disabling intro
    (cherry picked from commit cb8792a2ee6c0060d8151560959634b35d2fa11e)

--- game/LOAD/LOAD_TenStages.c
+++ game/LOAD/LOAD_TenStages.c
@@ -65,8 +65,10 @@ int LOAD_TenStages(struct GameTracker *gGT, int loadingStage, struct BigHeader *
 			sdata->boolFirstBoot = 0;
 
 			// Load Intro TIM for Copyright Page from VRAM file
-			LOAD_VramFile(bigfile, LOAD_FIRST_BOOT_COPYRIGHT_TIM_BIGFILE_INDEX, NULL, &vramSize, -1);
-			MainInit_VRAMDisplay();
+			if (!g_config.skipIntro) {
+				LOAD_VramFile(bigfile, LOAD_FIRST_BOOT_COPYRIGHT_TIM_BIGFILE_INDEX, NULL, &vramSize, -1);
+				MainInit_VRAMDisplay();
+			}
 
 #ifdef CTR_NATIVE
 			// NOTE(aalhendi): SCEA is already held by XA playback in MainMain. The copyright
